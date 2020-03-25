020b27555c4d23754c4d476a209b4edb0b2fbc83
bori <szilagyiborbala8@gmail.com>
2020-03-25 22:21:26 +0100

    alarm implementation and unit tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,6 +1,8 @@
 package hu.bme.mit.train.sensor;
 
-
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,15 +10,40 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainUser user;
+    TrainSensor sensor;
 
     @Before
     public void before() {
         // TODO Add initializations
-
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
-    public void SpeedLimitTest() {
-        //
+    public void TestCase1() {
+        sensor.overrideSpeedLimit(-1);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestCase2() {
+        sensor.overrideSpeedLimit(501);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestCase3() {
+        sensor.overrideSpeedLimit(sensor.getSpeedLimit()/4);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestCase4() {
+        sensor.overrideSpeedLimit(sensor.getSpeedLimit());
+        verify(user, times(0)).setAlarmState(true);
     }
 }
+
